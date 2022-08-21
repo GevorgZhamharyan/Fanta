4402d13fb650ea23a429e3b5ca0d95985bf3a8a3
Gevorg Zhamharyan <gevorgzhamharyan@outlook.com>
2022-08-21 21:52:36 +0400

    Fantastic!

---
+++ Fantastic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fantastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
